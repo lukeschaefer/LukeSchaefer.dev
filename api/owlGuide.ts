bf7651a851c310e8463cdc80280eb1d53823536c
Luke <yathern@gmail.com>
2026-09-06 19:03:23 -0400

    poop in my pants

--- api/owlGuide.ts
+++ api/owlGuide.ts
@@ -1,11 +1,6 @@
-import { createRequire } from "node:module";
-import path from "node:path";
+import { Path2D, createCanvas } from "@napi-rs/canvas";
 import { OWL_CANVAS_SIZE } from "./owlCircles";
 import { pencilfyOwl, type OwlCircle, type PencilCanvas } from "../src/utils/pencilfyOwl";
-
-const { Path2D, createCanvas } = createRequire(path.join(process.cwd(), "package.json"))(
-	"@napi-rs/canvas",
-) as typeof import("@napi-rs/canvas");
 
 export async function renderOwlGuideDataUrl(circles: OwlCircle[]): Promise<string> {
 	(globalThis as unknown as { Path2D: typeof Path2D }).Path2D = Path2D;
