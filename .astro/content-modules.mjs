7dc79931aca2bfc5684ae6e1399655be0bd8d724
Luke <yathern@gmail.com>
2026-09-06 16:37:54 -0400

    Finished post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/post/draw-the-owl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fdraw-the-owl.mdx&astroContentModuleFlag=true")]]);
+["src/content/post/draw-the-owl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fdraw-the-owl.mdx&astroContentModuleFlag=true")],
+["src/content/post/draw-the-owl-remux.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fdraw-the-owl-remux.mdx&astroContentModuleFlag=true")]]);
 		
